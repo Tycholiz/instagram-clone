b2e2b5a96231e849d5c1fbe9de48587ba9fa0bed
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-10-23 16:54:52 -0700

    add async comment storage

--- App.js
+++ App.js
@@ -7,10 +7,13 @@ import {
   View,
   Platform,
   Modal,
+  AsyncStorage, //a simple key-value store provided by React Native for storing small quantities of string data. saving and reading from this store both happen asynchronously
 } from 'react-native';
 
 import Feed from './screens/Feed';
 import Comments from './screens/Comments';
+
+const ASYNC_STORAGE_COMMENTS_KEY = 'ASYNC_STORAGE_COMMENTS_KEY';
 
 export default class App extends React.Component {
   state = {
@@ -19,7 +22,21 @@ export default class App extends React.Component {
     selectedItemId: null, //id of img we're storing comments for
   };
 
-  openCommentScreen = id => { //we need to call this from within CardList in order to pass the ID. then we propogate the value thru Feed and up to App.
+  async componentDidMount() {
+    try {
+      const commentsForItem = await AsyncStorage.getItem( //getItem means retrieving data from storage
+        ASYNC_STORAGE_COMMENTS_KEY,
+      );
+
+      this.setState({
+        commentsForItem: commentsForItem ? JSON.parse(commentsForItem) : {},
+      });
+    } catch (e) {
+      console.log('failed to load comments, message:', e)
+    }
+  }
+
+  openCommentScreen = id => { //we need to call this from within CardList in order to pass the ID. then we propogate the value thru Feed and up to App. Data is stored as stringified JSON, therefore when we store it we must stringify (serialize) it, and when we retrieve it, we must parse it.
     this.setState({
       showModal: true,
       selectedItemId: id,
@@ -44,6 +61,12 @@ export default class App extends React.Component {
     };
 
     this.setState({ commentsForItem: updated })
+
+    try {
+      AsyncStorage.setItem(ASYNC_STORAGE_COMMENTS_KEY, JSON.stringify(updated)); //setItem means putting data into storage
+    } catch(e) {
+      console.log(`failed to save comment ${text} for ${selectedItemId}`); //Note that getItem and setItem can both fail (e.g. when disk I/O fails), so we need to wrap any async calls in try/catch .
+    }
   };
 
   render() {
